4497df2899d48a5c4db2e7583aeb7234b45221b0
avi <63222803+avielbr@users.noreply.github.com>
2022-11-26 16:30:14 +0200

    .

---
+++ ch04/references_borrowing/target/debug/deps/references_borrowing-c5aa3d7523ca4fe2.d
@@ -0,0 +1,5 @@
+/home/avi/Documents/GitHub/rust-lang-book/ch04/references_borrowing/target/debug/deps/references_borrowing-c5aa3d7523ca4fe2: src/main.rs
+
+/home/avi/Documents/GitHub/rust-lang-book/ch04/references_borrowing/target/debug/deps/references_borrowing-c5aa3d7523ca4fe2.d: src/main.rs
+
+src/main.rs:
---
+++ ch04/references_borrowing/target/debug/references_borrowing.d
@@ -0,0 +1,1 @@
+/home/avi/Documents/GitHub/rust-lang-book/ch04/references_borrowing/target/debug/references_borrowing: /home/avi/Documents/GitHub/rust-lang-book/ch04/references_borrowing/src/main.rs
